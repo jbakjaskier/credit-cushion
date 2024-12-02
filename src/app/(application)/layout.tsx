b774be77fef541ee3c904421d326a83abd8c13f9
Gary Kumar <trngry814@github.com>
2024-12-02 23:16:13 +1100

    Move fonts to config folder

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -1,27 +1,17 @@
 import ApplicationHeader from "@/components/ApplicationHeader";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "../globals.css";
 import { ErrorBoundary } from "@/components/common/ErrorBoundary";
 import { APP_NAME, APP_DESCRIPTION } from "@/config/constants";
+import { getFontClasses } from "@/config/fonts";
 
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
+// Metadata configuration
 export const metadata: Metadata = {
   title: APP_NAME,
   description: APP_DESCRIPTION,
 };
 
+// Layout component
 export default function ApplicationLayout({
   children,
 }: Readonly<{
@@ -29,13 +19,11 @@ export default function ApplicationLayout({
 }>) {
   return (
     <html className="h-full bg-gray-100" lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
-        suppressHydrationWarning
-      >
+      <body className={`${getFontClasses()} h-full`} suppressHydrationWarning>
         <ErrorBoundary>
           <div className="min-h-full bg-gray-100">
             <ApplicationHeader />
+            
             <main className="-mt-32">
               <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
                 <div className="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "../globals.css";
 import MarketingHeader from "@/components/MarketingHeader";
 import { APP_NAME, APP_DESCRIPTION } from "@/config/constants";
-
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { getFontClasses } from "@/config/fonts";
 
 export const metadata: Metadata = {
   title: APP_NAME,
@@ -27,10 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        suppressHydrationWarning
-      >
+      <body className={getFontClasses()} suppressHydrationWarning>
         <div className="bg-white">
           <MarketingHeader />
           {children}
