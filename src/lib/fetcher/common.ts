77edaed3be936cf917bbd849fc5e094de7fa01a7
Jason Bakthakumar <jason@jaskier.co.nz>
2024-12-11 23:00:40 +1300

    immi

--- src/lib/fetcher/common.ts
+++ src/lib/fetcher/common.ts
@@ -1,10 +1,10 @@
-import { EnvelopeSentResult } from "./envelope";
+import { EnvelopeCreatedResult } from "./envelope";
 import { EnvelopeTemplate } from "./template";
 
 export type FetcherError = {
     errorMessage: string;
 }
 
-export function isFetcherError(input: FetcherError | EnvelopeTemplate[] | EnvelopeSentResult): input is FetcherError {
+export function isFetcherError(input: FetcherError | EnvelopeTemplate[] | EnvelopeCreatedResult): input is FetcherError {
     return (input as FetcherError).errorMessage !== undefined && (input as FetcherError).errorMessage !== null;
 }
